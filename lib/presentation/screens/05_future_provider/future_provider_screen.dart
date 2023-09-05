708cfbe5834a487a58542ee1c743d82dd71d4543
Miguel Sanchez <mangelsr25@gmail.com>
2023-09-05 13:23:35 -0500

    [UPD] FutureProviderScreen to interact with the new providers

--- lib/presentation/screens/05_future_provider/future_provider_screen.dart
+++ lib/presentation/screens/05_future_provider/future_provider_screen.dart
@@ -1,21 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import 'package:riverpod_app/presentation/providers/future_providers.dart';
 
-class FutureProviderScreen extends StatelessWidget {
+class FutureProviderScreen extends ConsumerWidget {
   const FutureProviderScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final pokemonName = ref.watch(pokemonNameProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Future Provider'),
       ),
-      body: const Center(
-        child: Text('Fernando Herrera'),
+      body: Center(
+        child: pokemonName.when(
+          data: (data) => Text(data),
+          loading: () => const CircularProgressIndicator(),
+          error: (error, stackTrace) => Text('$error'),
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: const Icon( Icons.refresh ),
-        onPressed: () {  },
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 'btn-prev',
+            child: const Icon(Icons.arrow_back),
+            onPressed: () {
+              ref.read(pokemonIdProvider.notifier).previousPokemon();
+            },
+          ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            heroTag: 'btn-next',
+            child: const Icon(Icons.arrow_forward),
+            onPressed: () {
+              ref.read(pokemonIdProvider.notifier).nextPokemon();
+            },
+          ),
+        ],
       ),
     );
   }
